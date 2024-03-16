fb23e9049e166ca05d7fc41783338d62d80231d3
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-03-16 20:59:13 +0900

    그래프 dfs bfs 다뤄보기 아직 많이 어설픔, 이중배열 진짜 어케하노?

---
+++ 3-16 (2).c
@@ -0,0 +1,457 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE	1
+#define FALSE	0
+#define STACK_LEN	10001
+#define QUE_LEN	10001
+
+typedef int Data;
+typedef int LData;
+
+typedef struct _cQueue
+{
+	int front;
+	int rear;
+	Data queArr[QUE_LEN];
+}Queue;
+
+typedef struct _arrayStack
+{
+	Data stackArr[STACK_LEN];
+	int topIndex;
+}Stack;
+
+typedef struct _node
+{
+	LData data;
+	struct _node * next;
+} Node;
+
+typedef struct _linkedList
+{
+	Node * head;
+	Node * cur;
+	Node * before;
+	int numOfData;
+	int (*comp)(LData d1, LData d2);
+	int parentnum;
+}List;
+
+// 리스트 관련 인자
+void ListInit(List * plist);
+void LInsert(List * plist, LData data);
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
+LData LRemove(List * plist);
+int LCount(List * plist);
+void SetSortRule(List * plist, int (*comp)(LData d1, LData d2)); //사용할 것
+
+// 스택 관련 인자들
+void StackInit(Stack * pstack);
+int SIsEmpty(Stack * pstack);
+void SPush(Stack * pstack, Data data);
+Data SPop(Stack * pstack);
+Data SPeek(Stack * pstack);
+typedef int LData;
+
+// 큐 관련 인자들
+void QueueInit(Queue * pq);
+int QIsEmpty(Queue * pq);
+void Enqueue(Queue * pq, Data data);
+Data Dequeue(Queue * pq);
+Data QPeek(Queue * pq);
+
+
+typedef struct _ual
+{
+	int numV;   // 정점의 수
+	int numE;   // 간선의 수
+	List * adjList;   // 간선의 정보
+	int * visitInfo;
+    int num;
+} ALGraph;
+
+// 그래프의 초기화
+void GraphInit(ALGraph * pg, int nv);
+// 그래프의 리소스 해제
+void GraphDestroy(ALGraph * pg);
+// 간선의 추가
+void AddEdge(ALGraph * pg, int fromV, int toV);
+// 유틸리티 함수: 간선의 정보 출력
+void ShowGraphEdgeInfo(ALGraph * pg);
+// Breadth First Search: 정점의 정보 출력
+void BFShowGraphVertex(ALGraph * pg, int startV);
+
+
+int WhoIsPrecede(int data1, int data2)
+{
+	if(data1 < data2)
+		return 0;
+	else
+		return 1;
+}
+
+// 1부터 시작으로 되어있음
+int main(void)
+{
+    int N,num1,num2;
+    scanf("%d",&N);
+
+    ALGraph graph;
+    GraphInit(&graph, N);
+
+    for(int i = 0 ; i < N-1 ;i++)
+    {
+        scanf("%d %d",&num1,&num2);
+        AddEdge(&graph, num1, num2);
+    }
+
+    BFShowGraphVertex(&graph,1);
+    
+    for(int i = 1 ; i < N ;i++)
+    {
+        printf("%d\n",graph.adjList[i].parentnum);
+    }
+    
+    
+
+    GraphDestroy(&graph);
+
+    return 1;
+    
+}
+
+
+
+void ListInit(List * plist)
+{
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+void FInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head->next;
+	plist->head->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+void SInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	Node * pred = plist->head;
+	newNode->data = data;
+
+	while(pred->next != NULL &&
+		plist->comp(data, pred->next->data) != 0)
+	{
+		pred = pred->next;
+	}
+
+	newNode->next = pred->next;
+	pred->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+
+void LInsert(List * plist, LData data)
+{
+	if(plist->comp == NULL)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+
+int LFirst(List * plist, LData * pdata)
+{
+	if(plist->head->next == NULL)
+		return FALSE;
+
+	plist->before = plist->head;
+	plist->cur = plist->head->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+int LNext(List * plist, LData * pdata)
+{
+	if(plist->cur->next == NULL)
+		return FALSE;
+
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+LData LRemove(List * plist)
+{
+	Node * rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
+
+int LCount(List * plist)
+{
+	return plist->numOfData;
+}
+
+void SetSortRule(List * plist, int (*comp)(LData d1, LData d2))
+{
+	plist->comp = comp;
+}
+
+void StackInit(Stack * pstack)
+{
+	pstack->topIndex = -1;
+}
+
+int SIsEmpty(Stack * pstack)
+{
+	if(pstack->topIndex == -1)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack * pstack, Data data)
+{
+	pstack->topIndex += 1;
+	pstack->stackArr[pstack->topIndex] = data;
+}
+
+Data SPop(Stack * pstack)
+{
+	int rIdx;
+
+	if(SIsEmpty(pstack))
+	{
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	rIdx = pstack->topIndex;
+	pstack->topIndex -= 1;
+
+	return pstack->stackArr[rIdx];
+}
+
+Data SPeek(Stack * pstack)
+{
+	if(SIsEmpty(pstack))
+	{
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	return pstack->stackArr[pstack->topIndex];
+}
+
+void QueueInit(Queue * pq)
+{
+	pq->front = 0;
+	pq->rear = 0;
+}
+
+int QIsEmpty(Queue * pq)
+{
+	if(pq->front == pq->rear)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+int NextPosIdx(int pos)
+{
+	if(pos == QUE_LEN-1)
+		return 0;
+	else
+		return pos+1;
+}
+
+void Enqueue(Queue * pq, Data data)
+{
+	if(NextPosIdx(pq->rear) == pq->front)
+	{
+		printf("Queue Memory Error!");
+		exit(-1);
+	}
+
+	pq->rear = NextPosIdx(pq->rear);
+	pq->queArr[pq->rear] = data;
+}
+
+Data Dequeue(Queue * pq)
+{
+	if(QIsEmpty(pq))
+	{
+		printf("Queue Memory Error!");
+		exit(-1);
+	}
+
+	pq->front = NextPosIdx(pq->front);
+	return pq->queArr[pq->front];
+}
+
+Data QPeek(Queue * pq)
+{
+	if(QIsEmpty(pq))
+	{
+		printf("Queue Memory Error!");
+		exit(-1);
+	}
+
+	return pq->queArr[NextPosIdx(pq->front)];
+}
+
+
+
+
+
+
+void GraphInit(ALGraph * pg, int nv)
+{
+	int i;	
+
+	pg->adjList = (List*)malloc(sizeof(List)*nv);
+	pg->numV = nv;
+	pg->numE = 0;     // 초기의 간선 수는 0개
+    pg->num = 1;
+	for(i=0; i<nv; i++)
+	{
+		ListInit(&(pg->adjList[i]));
+		SetSortRule(&(pg->adjList[i]), WhoIsPrecede); 
+	}
+
+	// 탐색 정보 등록을 위한 공간의 할당 및 초기화
+	pg->visitInfo= (int *)malloc(sizeof(int) * pg->numV);
+	memset(pg->visitInfo, 0, sizeof(int) * pg->numV);
+}
+
+
+// 그래프의 리소스 해제
+void GraphDestroy(ALGraph * pg)
+{
+	if(pg->adjList != NULL)
+		free(pg->adjList);
+
+	if(pg->visitInfo != NULL)
+		free(pg->visitInfo);
+}
+
+// 간선의 추가
+void AddEdge(ALGraph * pg, int fromV, int toV)
+{
+	LInsert(&(pg->adjList[fromV-1]), toV-1);
+	LInsert(&(pg->adjList[toV-1]), fromV-1);
+	pg->numE += 1;
+}
+
+// 유틸리티 함수: 간선의 정보 출력
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+	int i;
+	int vx;
+
+	for(i=0; i<pg->numV; i++)
+	{
+		printf("%d와 연결된 정점: ", i+1);
+		
+		if(LFirst(&(pg->adjList[i]), &vx))
+		{
+			printf("%d ", vx+1);
+			
+			while(LNext(&(pg->adjList[i]), &vx))
+				printf("%d ", vx+1);
+		}
+		printf("\n");
+	}
+}
+
+int VisitVertex(ALGraph * pg, int visitV, int parentV)
+{
+	if(pg->visitInfo[visitV] == 0)
+	{
+	    
+	    
+		pg->visitInfo[visitV] = 1;
+		pg->adjList[visitV].parentnum = parentV + 1;
+		//printf("%d ", visitV+1);     // 방문 정점 출력
+		return TRUE;
+	}
+	
+	return FALSE;
+}
+
+// Breadth First Search: 정점의 정보 출력
+void BFShowGraphVertex(ALGraph * pg, int startV)
+{
+	Queue queue;
+	int visitV = startV - 1;
+	int nextV;
+	int result = -1;
+	int parentV;
+
+	// DFS를 위한 큐의 초기화
+	QueueInit(&queue);
+
+	// 시작 정점 탐색
+	VisitVertex(pg, visitV, parentV);
+
+	while(LFirst(&(pg->adjList[visitV]), &nextV) == TRUE)
+	{
+	    
+		if(VisitVertex(pg, nextV, visitV) == TRUE)
+		{
+		    Enqueue(&queue, nextV);
+		}
+        
+
+		while(LNext(&(pg->adjList[visitV]), &nextV) == TRUE)
+		{
+
+			if(VisitVertex(pg, nextV, visitV) == TRUE)
+			{
+			    Enqueue(&queue, nextV);
+			}
+		}
+        
+        
+        
+		if(QIsEmpty(&queue) == TRUE)    // 큐가 비면 BFS 종료
+			break;
+		else
+		{
+			visitV = Dequeue(&queue);	
+		}
+	}
+
+	// 탐색 정보 초기화
+	memset(pg->visitInfo, 0, sizeof(int) * pg->numV);
+	
+	return result;
+}
+
+
+
+
+
+
+
+
